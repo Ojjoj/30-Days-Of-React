524bbc3f11f4e263d3fbe76e3f4633a601d37088
Ojjoj <karamkassem133@gmail.com>
2025-04-11 17:25:07 +0300

    day1 level03 completed

---
+++ solutions/day01/level02/script.js
@@ -0,0 +1,84 @@
+//dog = {}
+
+// print the dog object
+//console.log(dog)
+
+// fill the dog object
+// dog.name = "dawg"
+// dog.legs = 4
+// dog.color = "red"
+// dog.bark = function() { return "wouf wouf" }
+// dog.alive = true
+
+// console.log(dog["name"])
+// console.log(dog.bark())
+// console.log("Is the dog alive : " + dog.alive ? "yes" : "no")
+// console.log(dog.legs)
+
+// Ex2
+// const users = {
+//   Alex: {
+//     email: 'alex@alex.com',
+//     skills: ['HTML', 'CSS', 'JavaScript'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 30
+//   },
+//   Asab: {
+//     email: 'asab@asab.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//     age: 25,
+//     isLoggedIn: false,
+//     points: 50
+//   },
+//   Brook: {
+//     email: 'daniel@daniel.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//     age: 30,
+//     isLoggedIn: true,
+//     points: 50
+//   },
+//   Daniel: {
+//     email: 'daniel@alex.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   },
+//   John: {
+//     email: 'john@john.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//     age: 20,
+//     isLoggedIn: true,
+//     points: 50
+//   },
+//   Thomas: {
+//     email: 'thomas@thomas.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   },
+//   Paul: {
+//     email: 'paul@paul.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   }
+// }
+
+// function mostSkills(users){
+//     let mostSkills = 0
+//     let mostUser = users[0]
+//     for(let user in users){
+//         if(users[user].skills.length > mostSkills){
+//             mostSkills = users[user].skills.length
+//             mostUser = users[user]
+//         }
+//     }
+//     return mostUser
+// }
+
+// let user = mostSkills(users)
+// console.log(user.email)
