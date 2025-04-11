87b114a29b2d5ea6462c9936ac302f2c5f726579
Ojjoj <karamkassem133@gmail.com>
2025-04-11 15:37:05 +0300

    level01 completed

---
+++ solutions/day01/level01/script.js
@@ -0,0 +1,38 @@
+const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  getFullName: function (a) {
+    return `${this.firstName} ${a} ${this.lastName}`
+  },
+  'phone number': '+3584545454545',
+}
+
+console.log(person.getFullName("ali"))
+// accessing values using .
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+console.log(person.location)
+
+// value can be accessed using square bracket and key name
+console.log(person['firstName'])
+console.log(person['lastName'])
+console.log(person['age'])
+console.log(person['age'])
+console.log(person['location'])
+
+// for instance to access the phone number we only use the square bracket method
+console.log(person['phone number'])
